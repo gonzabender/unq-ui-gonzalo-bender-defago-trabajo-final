1d6e7ced4e0b61c8785b6195415b8d42fc34ff62
gonzabender <gonzabender@gmail.com>
2024-06-28 15:58:56 -0300

    Se resalta la opcion correcta

--- src/containers/question/question.jsx
+++ src/containers/question/question.jsx
@@ -5,18 +5,21 @@ import { useNavigate, useParams } from "react-router-dom";
 function Question() {
   const [preguntas, setPreguntas] = useState([]);
   const [completed, setCompleted] = useState(false)
+  const [correct, setCorrect] = useState("")
+  
   const navigate = useNavigate();
   const params = useParams();
 
   useEffect(() => {
-    const a = getAQuestion();
-    console.log(a)
+    
+    getAQuestion();
   }, [completed]);
 
   async function getAQuestion() {
     try {
       const response = await Api.questionByDifficulty(`${params.difficulty}`);
       setPreguntas(response);
+      setCorrect("")
       console.log(response);
     } catch (error) {
       console.error("Error fetching question:", error);
@@ -25,13 +28,15 @@ function Question() {
 
   const sendAnswer = (questionId, option) => {
     Api.answerQuestion(questionId, option)
-    .then((response) => handleResponse(response.data))
+    .then((response) => handleResponse(response.data,option))
     .catch((response) => "Error" + response)
   }
 
-  const handleResponse = (response) => {
+  const handleResponse = (response, option) => {
     response.answer == true && 
-    (console.log("Respuesta correcta"),  setCompleted(!completed))
+    (setCorrect(option), setTimeout(() => {
+      setCompleted(!completed);
+    }, 700))
   }
 
   return (
@@ -44,24 +49,28 @@ function Question() {
           {"Difficulty: "} {`${params.difficulty}`}
         </p>
         <div className="preguntas">
-          <button
+          <button style={"option1" == correct ? {background:"green"} : {}}
             onClick={() => {
               sendAnswer(`${preguntas[0].id}`,`option1`)
             }}
           >
             {preguntas[0]?.option1}
           </button>
-          <button onClick={() => {
+          <button 
+          style={"option2" == correct ? {background:"green"} : {}}
+          onClick={() => {
               sendAnswer(`${preguntas[0].id}`,`option2`)
             }}>
             {preguntas[0]?.option2}
           </button>
-          <button onClick={() => {
+          <button style={"option3" == correct ? {background:"green"} : {}}
+              onClick={() => {
               sendAnswer(`${preguntas[0].id}`,`option3`)
             }}>
             {preguntas[0]?.option3}
           </button>
-          <button onClick={() => {
+          <button style={"option4" == correct ? {background:"green"} : {}}
+          onClick={() => {
               sendAnswer(`${preguntas[0].id}`,`option4`)
             }}>
             {preguntas[0]?.option4}
